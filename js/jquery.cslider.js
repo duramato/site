7d9fd336e5e2554a17d51cff86f0939d3652fefc
supergonkas <matigonkas@outlook.com>
2016-06-24 14:11:51 +0100

    Fix time

--- js/jquery.cslider.js
+++ js/jquery.cslider.js
@@ -15,7 +15,7 @@
         current: 0, // index of current slide
         bgincrement: 100, // increment the bg position (parallax effect) when sliding
         autoplay: true, // slideshow on / off
-        interval: 6 // time between transitions
+        interval: 2000 // time between transitions
     };
 
     $.Slider.prototype = {
